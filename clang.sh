f772c06e3d496ef438bc54a1dd1524ae5ce6e65f
fredslbn <fh.silaban@gmail.com>
2024-10-11 23:14:11 +0700

    build script

--- clang.sh
+++ clang.sh
@@ -12,19 +12,20 @@ DEVICE=gki
 # Kernel Defconfig
 export DEFCONFIG=gki_defconfig
 
-export CROSS_COMPILE=$KERNEL_DIR/clang-r530567/bin/aarch64-linux-gnu-
-export CC=$KERNEL_DIR/clang-r530567/bin/clang
+export CROSS_COMPILE=$KERNEL_DIR/WeebX-Clang-19.1.2/bin/aarch64-linux-gnu-
+export CC=$KERNEL_DIR/WeebX-Clang-19.1.2/bin/clang
 
-export PATH=$KERNEL_DIR/clang-r530567/bin:$PATH
+export PATH=$KERNEL_DIR/WeebX-Clang-19.1.2/bin:$PATH
 export PATH=$KERNEL_DIR/build-tools/path/linux-x86:$PATH
-export PATH=$KERNEL_DIR/gas/linux-x86:$PATH
-export TARGET_SOC=s5e9925
-export LLVM=1 LLVM_IAS=1
+#export PATH=$KERNEL_DIR/gas/linux-x86:$PATH
+#export TARGET_SOC=s5e9925
+export LLVM=1 
+#export LLVM_IAS=1
 export ARCH=arm64
 export KBUILD_BUILD_HOST=Pancali
 export KBUILD_BUILD_USER="unknown"
 
-IMAGE="$(pwd)/arch/arm64/boot/Image"
+IMAGE="$(pwd)/arch/arm64/boot/Image.gz"
 
 export KERNEL_MAKE_ENV="LOCALVERSION=-SUPER.KERNEL.TOPAZ"
 
@@ -35,17 +36,17 @@ export ZIPNAME="SUPER.KERNEL.TOPAZ-(clang)-$(TZ=Asia/Jakarta date +"%Y%m%d-%H%M"
 
 
 clang(){
-  if [ ! -d $KERNEL_DIR/clang-r530567 ]; then
-  wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r530567.tar.gz && mkdir clang-r530567 && tar -xzf clang-r530567.tar.gz -C clang-r530567/
+  if [ ! -d $KERNEL_DIR/WeebX-Clang-19.1.2 ]; then
+  wget https://github.com/XSans0/WeebX-Clang/releases/download/WeebX-Clang-19.1.2-release/WeebX-Clang-19.1.2.tar.gz && mkdir WeebX-Clang-19.1.2 && tar -xzf WeebX-Clang-19.1.2.tar.gz -C WeebX-Clang-19.1.2/
   CLANG_VERSION=$(clang --version | grep version | sed "s|clang version ||")
   fi
 }
 
-gas(){
-  if [ ! -d $KERNEL_DIR/gas/linux-x86 ]; then
-    git clone https://android.googlesource.com/platform/prebuilts/gas/linux-x86 $KERNEL_DIR/gas/linux-x86
-  fi
-}
+#gas(){
+#  if [ ! -d $KERNEL_DIR/gas/linux-x86 ]; then
+#    git clone https://android.googlesource.com/platform/prebuilts/gas/linux-x86 $KERNEL_DIR/gas/linux-x86
+#  fi
+#}
 
 build_tools(){
   if [ ! -d $KERNEL_DIR/build-tools ]; then
@@ -61,13 +62,13 @@ build_kernel() {
   make -j$(nproc) -C $(pwd) $KERNEL_MAKE_ENV
 
   echo "**** Verify Image ****"
-  ls $(pwd)/arch/arm64/boot/Image
+  ls $(pwd)/arch/arm64/boot/Image.gz
 
 }
 
 anykernel3() {
 
-cp $(pwd)/arch/arm64/boot/Image AnyKernel3
+cp $(pwd)/arch/arm64/boot/Image.gz AnyKernel3
 cd AnyKernel3 || exit 1
 zip -r9 ${ZIPNAME} *
 MD5CHECK=$(md5sum "$ZIPNAME" | cut -d' ' -f1)
@@ -81,7 +82,7 @@ cd ..
 
 # Run once
 clang
-gas
+#gas
 build_tools
 build_kernel
 anykernel3
